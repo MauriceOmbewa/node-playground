b26570093e5fb3d92c9cc6ca17ccccc5fc7f8b89
mombewa <ombewamaurice79@gmail.com>
2025-05-14 11:28:05 +0300

    feat: Add updateLog function to logController for updating existing logs

--- controllers/logController.js
+++ controllers/logController.js
@@ -9,3 +9,8 @@ exports.createLog = async (req, res) => {
     const newLog = Log.create(req.body);
     res.status(201).json(newLog);
 }
+
+exports.updateLog = async (req, res) => {
+    const updatedLog = await Log.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    res.json(updatedLog);
+}
