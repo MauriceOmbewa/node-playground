41816395d41e0f8a2d7f91ac0d2de513650566a6
mombewa <ombewamaurice79@gmail.com>
2025-05-14 11:21:20 +0300

    feat: Add createLog function to logController for creating new logs

--- controllers/logController.js
+++ controllers/logController.js
@@ -4,3 +4,8 @@ exports.getLogs = async (req, res) => {
     const logs = await Log.find();
     res.json(logs);
 }
+
+exports.createLog = async (req, res) => {
+    const newLog = Log.create(req.body);
+    res.status(201).json(newLog);
+}
